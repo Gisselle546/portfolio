2376aed675e626797873ccf4b6ea37704f5f4489
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2021-01-08 16:04:15 -0500

    updates

--- client/src/Components/Projects.tsx
+++ client/src/Components/Projects.tsx
@@ -16,7 +16,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
     list:{
       marginTop:'4rem',
-      boxShadow: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
+      boxShadow: "box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px";
       
     },
 
