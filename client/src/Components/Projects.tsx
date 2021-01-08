882c65531cd73130b025c1f2cb7279817180eee1
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2021-01-08 13:15:21 -0500

    update project page

--- client/src/Components/Projects.tsx
+++ client/src/Components/Projects.tsx
@@ -66,14 +66,14 @@ const Projects: React.FC=()=>{
     console.log(data);
 
         const dataSet = data!.projects.map((project)=>{
-          console.log(project.image[0])
+         
           
           return(
-            
+
             <div className={classes.list} key={project.id}>
               <Card className={classes.card}>
               <CardMedia
-              image={project.image[0]}
+              src={project.image[0]}
               title="Live from space album cover"
               />
                 
