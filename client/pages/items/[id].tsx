bcf7670e24f8156a9a94543a3553d78e682b8766
Gisselle Rodriguez <gissellerodriguez431@gmail.com>
2022-01-24 11:28:58 -0500

    updates

--- client/pages/items/[id].tsx
+++ client/pages/items/[id].tsx
@@ -34,8 +34,11 @@ const ProjectItem = ({data}:any) => {
 
 
     useEffect(()=>{
-      TLLOAD.from(leftcon.current,{autoAlpha: 0, y: -40, delay: 0.6, duration:1.5})
-            .from(rightcon.current,{x:40, duration: 0.6 })
+     
+       ()=>{
+        TLLOAD.from(leftcon.current,{autoAlpha: 0, y: -40, delay: 0.6, duration:1.5})
+        .from(rightcon.current,{x:40, duration: 0.6 })
+       }
     })
 
 
